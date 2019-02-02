26fc26ed485e337b3c2a3ab49ea97038e29a866d
Myorthy <46349046+Myorthy@users.noreply.github.com>
2019-02-02 09:55:14 +0500

    Update files.py
    доделал задачу 2

--- files.py
+++ files.py
@@ -1,3 +1,4 @@
+#Задача 1
 with open('dishes.txt', encoding='utf8') as list_dishes:
     cook_book = {}
     for dish in list_dishes:
@@ -10,6 +11,27 @@ with open('dishes.txt', encoding='utf8') as list_dishes:
             value_dict = {'ingridient_name': value[0], 'quantity': int(value[1]), 'measure': value[2]}
             values.append(value_dict)
         list_dishes.readline()
-        cook_book.update({ key : values})
+        cook_book.update({key : values})
     print(cook_book)
 
+#Задача 2
+# dishes = input('Введите названия блюд : ').split()
+# person_count = int(input('Введите количество персон : '))
+
+def get_shop_list_by_dishes(dishes, person_count):
+    order_list = {}
+    for dish in dishes:
+        if dish in cook_book.keys():
+            for book_dish, ingridients  in cook_book.items():
+                if book_dish == dish:
+                    for ingridient in ingridients:
+                        if ingridient['ingridient_name'] not in order_list.keys():
+                            key = ingridient['ingridient_name']
+                            value = {'measure': ingridient['measure'], 'quantity': ingridient['quantity'] * person_count}
+                            order_list.update({key : value})
+                        else:
+                            value['quantity'] += ingridient['quantity'] * person_count
+        else:
+            print(f'Выбранное блюдо: {dish} отсутствует в меню')
+    return order_list
+print(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
